0023b6adcb40d9ff4fb83e2d5ac26bf59c37784b
Antoni Spaanderman <56turtle56@gmail.com>
2021-10-03 17:33:40 +0200

    remove residual packages

--- debloater.sh
+++ debloater.sh
@@ -3,6 +3,9 @@
 # remove bloat
 apt remove --purge -y wolfram-engine libreoffice* scratch* minecraft-pi sonic-pi dillo gpicview oracle-java8-jdk openjdk-7-jre oracle-java7-jdk openjdk-8-jre smartsim python3-pygame python-pygame python-tk
 
+# remove residual packages (when you forget to --purge)
+apt remove --purge -y $(dpkg -l | grep "^rc" | awk '{print $2}')
+
 # extra remove steps
 rm -rf ~/python_games
 
