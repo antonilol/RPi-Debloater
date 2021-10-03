6f84e51e992717dda96e2e92c973aa7b7f07d365
Antoni Spaanderman <49868160+antonilol@users.noreply.github.com>
2021-10-03 15:59:00 +0200

    `-y` is an argument

--- debloater.sh
+++ debloater.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Remove bloatware (Wolfram Engine, Libre Office, Minecraft Pi, sonic-pi dillo gpicview penguinspuzzle oracle-java8-jdk openjdk-7-jre oracle-java7-jdk openjdk-8-jre smartsim python3-pygame python-pygame python-tk)
-sudo apt-get remove --purge wolfram-engine libreoffice* scratch* minecraft-pi sonic-pi dillo gpicview oracle-java8-jdk openjdk-7-jre oracle-java7-jdk openjdk-8-jre smartsim python3-pygame python-pygame python-tk-y
+sudo apt-get remove --purge wolfram-engine libreoffice* scratch* minecraft-pi sonic-pi dillo gpicview oracle-java8-jdk openjdk-7-jre oracle-java7-jdk openjdk-8-jre smartsim python3-pygame python-pygame python-tk -y
 
 # Autoremove
 sudo apt-get autoremove -y
