c7241f94b099883769aae73263ed44aeac05fa74
Antoni Spaanderman <56turtle56@gmail.com>
2021-10-06 21:34:05 +0200

    oops it was x bloat

--- xdebloater.sh
+++ xdebloater.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-apt remove --purge -y 'libx11-.*' xdg-user-dirs xdg-utils xkb-data 'alsa*'
+apt remove --purge -y 'libx11-.*' xdg-user-dirs xdg-utils xkb-data 'alsa*' lxde-icon-theme menu-xdg
 
