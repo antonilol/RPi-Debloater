9be178be57af53891abe77d240fe205c12c5b4cc
Antoni Spaanderman <56turtle56@gmail.com>
2021-10-06 18:01:54 +0200

    more bloat

--- debloater.sh
+++ debloater.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # remove bloat
-apt remove --purge -y wolfram-engine 'libreoffice*' 'scratch*' minecraft-pi sonic-pi dillo gpicview oracle-java8-jdk openjdk-7-jre oracle-java7-jdk openjdk-8-jre smartsim python3-pygame python-pygame python-tk exim4-base
+apt remove --purge -y wolfram-engine 'libreoffice*' 'scratch*' minecraft-pi sonic-pi dillo gpicview oracle-java8-jdk openjdk-7-jre oracle-java7-jdk openjdk-8-jre smartsim python3-pygame python-pygame python-tk exim4-base samba-common
 
 # remove residual packages (when you forget to --purge)
 apt remove --purge -y $(dpkg -l | grep "^rc" | awk '{print $2}')
